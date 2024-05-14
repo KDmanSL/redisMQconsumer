0760c26d9697bc8c0f311b104fd9db52dec887d7
韶光善良君 <kindness-man@outlook.com>
2024-05-14 13:34:47 +0800

    消费者添加延迟，修改保存信息到本地

--- src/main/java/com/mqconsumer/listener/SpringRabbitListener.java
+++ src/main/java/com/mqconsumer/listener/SpringRabbitListener.java
@@ -3,11 +3,14 @@ package com.mqconsumer.listener;
 import com.baidu.aip.ocr.AipOcr;
 import com.mqconsumer.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,9 +19,18 @@ import java.util.Map;
 public class SpringRabbitListener {
     @RabbitListener(queues = "baidu.ai.mq")
     public void listenSimpleQueue(Map msg){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            log.error("线程休眠异常，{}", e.getMessage());
+        }
         String img_info = imgHandlerAI(msg.get("file_url").toString());
-        System.out.println("uuid: "+ msg.get("uuid"));
-        System.out.println("img_info: "+ img_info);
+        String uuid = msg.get("uuid").toString();
+
+        log.info("uuid:{}, img_info:{}", uuid, img_info);
+
+        // 结果保存到本地
+        saveToCsv(uuid, img_info);
 
     }
 
@@ -40,5 +52,21 @@ public class SpringRabbitListener {
             log.error("获取json数据异常，{}", e.getMessage());
         }
         return jsonData;
+    }
+    private void saveToCsv(String uuid, String imgInfo) {
+        boolean isFileNew = false;
+        try (FileOutputStream fos = new FileOutputStream("output.csv", true);
+             Writer writer = new OutputStreamWriter(fos, "UTF-8");
+             CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
+
+            if (new File("output.csv").length() == 0) {
+                csvPrinter.printRecord("UUID", "Image Info");
+                isFileNew = true;
+            }
+            csvPrinter.printRecord(uuid, imgInfo);
+            csvPrinter.flush();
+        } catch (IOException e) {
+            log.error("写入CSV文件时发生错误: {}", e.getMessage());
+        }
     }
 }
